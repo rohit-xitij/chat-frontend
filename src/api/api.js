45048cdc61539f4408f77fa7c1730ee128342b5a
rohit <rohitkalgude.xitijinfo@gmail.com>
2026-03-25 10:40:08 +0530

    change

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: "https://chat-backend-6r6t.onrender.com",
+  baseURL: "https://chat-backend-6r6t.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",

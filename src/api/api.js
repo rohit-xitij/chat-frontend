93fde21b18744ff76a817b0f6c3d7a8fd9bd68df
rohit <rohitkalgude.xitijinfo@gmail.com>
2026-03-25 09:49:01 +0530

    baseurl change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://chat-backend-6r6t.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
